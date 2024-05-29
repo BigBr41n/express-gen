47aa86dd05b193352e45d724e4164fbe3f65e706
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-05-29 22:47:53 +0200

    temp push

---
+++ bin/Express-Boilerplate-Generator.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env ts-node
+
+import "ts-node/register";
+import { createExpressProject } from "../commands/create";
+
+const projectName = process.argv[2];
+
+if (!projectName) {
+  console.error("Please provide a project name.");
+  process.exit(1);
+}
+
+createExpressProject(projectName);
