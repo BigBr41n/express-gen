f0002ab616052e0753c91f288449e6f94721a8bd
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-06-08 14:31:46 +0200

    v1.0.1

--- OUTPUT-example/src/router.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-function routes(app: Express) {
-  app.get("/api/v1/healthCheck", (req, res) =>
-    res.status(200).json("Server is On")
-  );
-}
-export default routes;
